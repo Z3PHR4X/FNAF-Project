f99ab21841fd46c53090ca5fcf76004e5ead3316
Zephrax <Z3PHR4X@users.noreply.github.com>
2023-12-23 09:03:11 +0100

    Simple script for switching materials when going into seccams

---
+++ Assets/Scripts/Tools/SwapMaterialsOnCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools
+{
+    public class SwapMaterialsOnCam : MonoBehaviour
+    {
+        [SerializeField] private Material defaultMaterial, cameraMaterial;
+        private SkinnedMeshRenderer meshRenderer;
+        private bool camState;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            meshRenderer = GetComponent<SkinnedMeshRenderer>();
+            SwapMaterial(defaultMaterial);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (camState != Player.Instance.isInCamera)
+            {
+                camState = Player.Instance.isInCamera;
+                if (camState)
+                {
+                    SwapMaterial(cameraMaterial);
+                }
+                else
+                {
+                    SwapMaterial(defaultMaterial);
+                }
+            }
+        }
+
+        void SwapMaterial(Material mat)
+        {
+            meshRenderer.material = mat;
+        }
+    }
+}
